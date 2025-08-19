73e1e6fe4ec5e04154aede629c02848aa436ff72
shhivam07 <shivam07singh7781@gmail.com>
2025-08-19 22:35:39 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,7 @@
-const CACHE_NAME = "quiz-app-cache-v1";
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/index.js",
-  "/manifest.json"
-];
-
-// Install service worker
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+elf.addEventListener("install", event => {
+  console.log("Service Worker installed");
 });
 
-// Fetch cached files
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener("activate", event => {
+  console.log("Service Worker activated");
 });
